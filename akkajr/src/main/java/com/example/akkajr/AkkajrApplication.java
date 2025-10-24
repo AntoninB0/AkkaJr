e8c6f5da162f2c640321e81adee38984a7f2753a
Antonin <bo.antonin@gmail.com>
2025-10-24 11:23:22 +0200

    docker base + spring init thymelife

---
+++ akkajr/src/main/java/com/example/akkajr/AkkajrApplication.java
@@ -0,0 +1,13 @@
+package com.example.akkajr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkajrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkajrApplication.class, args);
+	}
+
+}
---
+++ akkajr/src/test/java/com/example/akkajr/AkkajrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.akkajr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkkajrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
