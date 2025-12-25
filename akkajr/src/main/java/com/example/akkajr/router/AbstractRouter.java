45e3a0287e62ccf98ab798ec618a87782f5896a6
assiabr <assia.brouzi@gmail.com>
2025-12-25 18:43:07 +0100

    modification pour Springboot et messaging routing

--- akkajr/src/main/java/com/example/akkajr/router/AbstractRouter.java
+++ akkajr/src/main/java/com/example/akkajr/router/AbstractRouter.java
@@ -1,25 +1,58 @@
 package com.example.akkajr.router;
 
-import com.example.akkajr.core.actors.Actor;
-import com.example.akkajr.core.actors.ActorRef;
+//import com.example.akkajr.core.actors.Actor;
+//import com.example.akkajr.core.actors.ActorRef;
+
+import com.example.akkajr.messaging.Message;
+import com.example.akkajr.messaging.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-public abstract class AbstractRouter extends Actor{
-	protected List<ActorRef> routes;
+public abstract class AbstractRouter { //class centrale
 	
-	public AbstractRouter(List<ActorRef> routes) {
-		this.routes = routes;
+	//injection du service de messagerie
+	@Autowired
+    protected MessageService messageService;
+	
+	// Liste des identifiants des workers vers lesquels router (ex: "workerA", "workerB")
+    protected List<String> routeIds;
+	
+	public AbstractRouter(List<String> routeIds) {
+		this.routeIds = routeIds;
 	}
 	
-	@Override
-	public void receive(Object message, ActorRef sender) throws Exception {
-        ActorRef target = selectRoute();
-        if (target != null) {
-            target.tell(message, sender);
+	public void route(Message message) {
+        // 1. Choisir la destination selon la stratégie (Round Robin, Broadcast, etc.)
+        String targetId = selectRoute();
+
+        if (targetId != null) {
+            // 2. Mettre à jour le destinataire du message
+            message.setReceiverId(targetId);
+            
+            // 3. Réinjecter le message dans le service pour l'envoi final
+            // Cela permet de profiter de l'historique, des dead letters et du remote.
+            messageService.send(message);
+        } else {
+            System.err.println("[ROUTER] Erreur : Aucune destination disponible pour le message " + message);
         }
     }
 	
-	protected abstract ActorRef selectRoute();
+	
+	/**
+     * Logique de sélection spécifique à chaque type de routeur.
+     * @return L'ID de l'agent destinataire (String).
+     */
+    protected abstract String selectRoute();
+    
+    // Getter et Setter pour permettre de modifier les workers dynamiquement (Bloc 7)
+    public List<String> getRouteIds() {
+        return routeIds;
+    }
+
+    public void setRouteIds(List<String> routeIds) {
+        this.routeIds = routeIds;
+    }
+	
 
 }
